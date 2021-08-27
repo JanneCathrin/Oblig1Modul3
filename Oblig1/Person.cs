6bac9154086a8701c702e799753d4acd55294284
JanneCathrin <77385680+JanneCathrin@users.noreply.github.com>
2021-08-27 14:18:27 +0200

    Initial commit

---
+++ Oblig1/Person.cs
@@ -0,0 +1,95 @@
+﻿namespace Oblig1
+{
+    public class Person
+    {
+        public int Id;
+        public string FirstName;
+        public string LastName;
+        public int? BirthYear;
+        public int? DeathYear;
+        public Person Father;
+        public Person Mother;
+
+
+        public string GetDescription()
+
+
+        {
+
+            var description = "";
+
+
+            if (FirstName != null) description += $"{FirstName} ";
+            if (LastName != null) description += $"{LastName} ";
+            if (Id != 0) description += $"(Id={Id})";
+            if (BirthYear != null) description += $" Født: {BirthYear}";
+            if (DeathYear != null) description += $" Død: {DeathYear}";
+            if (Father != null) description += $" Far: {Father.FirstName} " + $"(Id={Father.Id})";
+            if (Mother != null) description += $" Mor: {Mother.FirstName} " + $"(Id={Mother.Id})";
+            return description;
+
+        }
+
+            
+
+
+
+        }
+    }
+
+
+// if (Id == null)
+//{
+//  return $" (Id={Id})";
+//}
+
+
+//if (Father != null)
+//{
+//    description += $"Far: {Father.FirstName} ";
+//    description += $"(Id={Father.Id}) ";
+
+//}
+
+//if (Mother != null)
+//{
+//    description += $"Mor: {Mother.FirstName} ";
+//    description += $"(Id={Mother.Id})";
+//}
+
+//else if (Id > 0)
+//{
+//    return $"(Id={Id})";
+//}
+
+
+//-------------------------------------------------------------------------------------------------
+
+
+
+// description.($"{Id}");
+// if (description !=  )
+// {
+//   return $"(Id={Id})";
+//}
+
+
+//--------------------------------------------------------------------------------------------------
+
+
+//(description = !description.Add(Id)
+
+// description.Add($"(Id={Id})
+
+
+
+
+
+
+
+
+
+// "Ola Nordmann (Id=17) Født: 2000 Død: 3000 Far: Per (Id=23) Mor: Lise (Id=29)";
+
+
+
